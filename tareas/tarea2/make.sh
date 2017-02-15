65365665ca86388445c6436cb37a9820a89b3587
Emilio Rojas <emilio93@gmail.com>
2017-02-15 04:57:47 +0000

    updates

---
+++ tareas/tarea2/make.sh
@@ -0,0 +1,2 @@
+cp main.pdf T2_B15680_EmilioJavierRojasAlvarez.pdf
+zip -0 -T -m  T2_B15680_EmilioJavierRojasAlvarez.zip T2_B15680_EmilioJavierRojasAlvarez.pdf T2_B15680_EmilioJavierRojasAlvarez.circ
